9aeaca4e3d09771262e47514cb070ba7a69b3fb8
yonehara <naomi.yonehara@warc.jp>
2020-08-31 15:45:53 +0900

    Initial commit

---
+++ page-thankyou.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: 送信完了
+*/
+?>
+<?php get_header(); ?>
+    <div class="header_border"></div><div class="contents_block">
+      <div class="contents_blockInner">
+        <div id="thanks_cont">
+          <div class="layout_1080">
+            <div class="thanks_block">
+              <div class="thanks_blockInner">
+                <h1>
+                  送信完了
+                </h1>
+                <p>
+                  この度は、お問い合わせいただき、<br class="is-sp">誠にありがとうございました。<br>お送りいただきました内容を確認の上、<br class="is-sp">担当者より折り返しご連絡させていただきますので、<br>しばらくお待ちください。
+                </p>
+                <div class="btn_long">
+                  <a href="https://icuco.wkwk-c.com/">TOPに戻る</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php get_footer(); ?>
